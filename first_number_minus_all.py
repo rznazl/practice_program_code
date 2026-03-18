883e1c8b1bd974149140f7faaec709ded137ddd6
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-18 21:24:06 +0800

    subtract the numbers and show the result

--- first_number_minus_all.py
+++ first_number_minus_all.py
@@ -5,4 +5,7 @@ count = 2
 while count <= 10:
     num = float(input(f"Enter number {count}: "))
 #subtract the numbers
+    total -= num
+    count += 1
 #print the final result
+print(f"The result is: {total}")
