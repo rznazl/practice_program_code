deb50005e37ffeec21a35a9de8723cdc9d16db2b
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-18 21:21:35 +0800

    input the first and the rest of the numbers needed

--- first_number_minus_all.py
+++ first_number_minus_all.py
@@ -1,4 +1,8 @@
 #input the first number
+total = float(input("Enter number 1: "))
 #loop 9 times to get the remaining numbers
+count = 2
+while count <= 10:
+    num = float(input(f"Enter number {count}: "))
 #subtract the numbers
 #print the final result
