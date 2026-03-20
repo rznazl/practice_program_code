ce04c08429351f17567cb195be8ffca380f9cf6e
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-21 00:00:22 +0800

    count the odd numbers and show the result

--- odd_number_counter.py
+++ odd_number_counter.py
@@ -2,3 +2,8 @@ odd_count = 0
 
 print("Please enter 10 numbers")
 
+for i in range (1,11):
+    number = int(input(f"Enter number {i}: "))
+    if number % 2 != 0:
+        odd_count += 1
+print(f"The total od the odd numbers is {odd_count}.")
