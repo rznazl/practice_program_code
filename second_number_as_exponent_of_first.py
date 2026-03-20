b4cedacea84b18fede2edaee859233c130fde948
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-20 23:30:24 +0800

    calculate the answer then print

--- second_number_as_exponent_of_first.py
+++ second_number_as_exponent_of_first.py
@@ -1,2 +1,5 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+second_number_as_exponent = num1 ** num2
+
+print(f"The result is {second_number_as_exponent}.")
