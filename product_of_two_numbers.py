12320024582b36af2f33f26d02147d794b8aa4d1
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-20 23:19:33 +0800

    calculate the product and print the result

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -1,2 +1,5 @@
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
+product_of_2_numbers = num1 * num2
+
+print(f"The product of both numbers is {product_of_2_numbers}.")
