d9ca65e195442e4e7ece394884493e323add5ed8
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-20 23:02:17 +0800

    check if what number is bigger

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -1,3 +1,9 @@
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
 
+if num1 > num2:
+    print(f"The bigger number is {num1}.")
+elif num1 == num2:
+    print("Both numbers are equal.")
+else:
+    print(f"The bigger number is {num2}.")
