eff2df2cea949fecffcd2f9e4f14a732be1bfe99
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-20 23:24:31 +0800

    calculate the quotient with decimal and print the result

--- quotient_with_decimal.py
+++ quotient_with_decimal.py
@@ -1,2 +1,5 @@
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number: "))
+quotient_of_2_numbers = num1 / num2
+
+print(f"The quotient of both numbers is {quotient_of_2_numbers}.")
