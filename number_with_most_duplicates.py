9ac2a06340aa4751c2e7b784d19a69975427cb89
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-22 00:46:25 +0800

    monitor the count of each number that will be entered. update the track from time to time to identify the number with the most duplicates

--- number_with_most_duplicates.py
+++ number_with_most_duplicates.py
@@ -15,4 +15,11 @@ if not numbers:
     print("No numbers were detected.")
 else:
     most_frequent = numbers[0]
-    max_count = 0 
+    max_count = 0
+
+    for n in set(numbers):
+        current_count = numbers.count(n)
+
+        if current_count > max_count:
+            max_count = current_count
+            most_frequent = n
