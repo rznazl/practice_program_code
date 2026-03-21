c831af5fd1df64c64b6857e28b44d3489c4dd603
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-21 23:36:35 +0800

    convert the input to integer and see if what number is the lowest

--- infinite_loop_and_lowest_number.py
+++ infinite_loop_and_lowest_number.py
@@ -4,3 +4,9 @@ print("Enter any number. Enter any letter to stop the program.")
 
 while True:
     user_input = input("Enter a number: ")
+
+    try:
+        current_num = int(user_input)
+
+        if lowest is None or current_num < lowest:
+            lowest = current_num
