828ae9c8b6ccf7f7a028de0813c4a37b1fa1f283
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-22 00:38:15 +0800

    convert the input into integer if possible

--- number_with_most_duplicates.py
+++ number_with_most_duplicates.py
@@ -5,4 +5,8 @@ print("Enter numbers one by one. Enter any letter to stop.")
 while True:
     user_input = input("Enter a number: ")
 
-    
+    try:
+        current_num = float(user_input)
+        numbers.append(current_num)
+    except ValueError:
+        break
