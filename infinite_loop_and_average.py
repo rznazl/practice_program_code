78bb8a6c462ac7ac84981a96b7c3e8e2191814bd
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-22 01:20:17 +0800

    calculate the average of all the numbers entered. double check if there are there are any valid value entered or not

--- infinite_loop_and_average.py
+++ infinite_loop_and_average.py
@@ -1,7 +1,13 @@
-numbers: []
+numbers = []
 while True:
     value = input("Enter any number. Enter any letter to stop: ")
     try:
-        nums.append(int(value))
+        numbers.append(int(value))
     except ValueError:
         break
+
+if numbers:
+    average_of_all_numbers = sum(numbers) / len(numbers)
+    print(f"The Average of all the numbers is {average_of_all_numbers}.")
+else:
+    print("No numbers were entered.")
