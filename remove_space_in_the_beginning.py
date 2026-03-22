3b18360fec0841c47aba9dccc2434a7a2c8d76bf
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-22 23:21:53 +0800

    slice the string from the first non-space character until the end and print the result

--- remove_space_in_the_beginning.py
+++ remove_space_in_the_beginning.py
@@ -3,3 +3,7 @@ full_name = input("Enter your full name with spaces in the beginning: ")
 index = 0
 while index < len(full_name) and full_name[index] == " ":
     index += 1
+
+output = full_name[index:]
+
+print("Output: ", output)
