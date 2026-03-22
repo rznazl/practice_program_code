187e07405f60fe70f6e22b54c7d98878fcc89966
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-22 20:42:10 +0800

    convert the entered value into an integer when possible

--- infinite_loop_of_numbers_highest_to_lowest.py
+++ infinite_loop_of_numbers_highest_to_lowest.py
@@ -4,4 +4,9 @@ print("Enter any number. Enter any letter to stop the program.")
 
 while True:
     user_input = input("Enter here: ")
-    
+    try:
+        num = int(user_input)
+        number.append(num)
+    except ValueError:
+        break
+        
