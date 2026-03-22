fd86eab4bf0c5ba131645f9f4c4b90f9d07f1451
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-22 22:45:56 +0800

    convert the entered name in pascal case and show the result

--- full_name_in_pascal_case.py
+++ full_name_in_pascal_case.py
@@ -1,2 +1,4 @@
 full_name = input("Enter your full name in an incorrect casing (e.g., ReXi DiMaRaNaN): ")
 
+pascal_case = full_name.title().replace(" ", "")
+print("Full name in pascal case:", pascal_case)
