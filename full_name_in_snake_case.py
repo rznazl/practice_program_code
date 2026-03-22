2f98becc6d2481c7b6aef21e3e0398e24cab5b4e
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-22 22:52:56 +0800

    convert the entered name into snake case and print the result

--- full_name_in_snake_case.py
+++ full_name_in_snake_case.py
@@ -1,2 +1,4 @@
 full_name = input("Enter your name in incorrect casing (e.g., AiZeLlE dElOs ReYeS): ")
 
+snake_case = full_name.lower().replace(" ", "_")
+print("Full name in snake_case:", snake_case)
