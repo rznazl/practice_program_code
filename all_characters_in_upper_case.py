683179fc3059901e22dc9a7fdcd6bf51b8e383b3
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-23 23:47:47 +0800

    chcek if the characters entered is on lowercase. if it's already entered in uppercase, if it is a space or any other symbol, let it be. print the result

--- all_characters_in_upper_case.py
+++ all_characters_in_upper_case.py
@@ -3,3 +3,10 @@ upper_case_name = ""
 
 for char in full_name:
     ascii_val = ord(char)
+
+    if 97 <= ascii_val <= 122:
+        upper_case_name += chr(ascii_val - 32)
+    else:
+        upper_case_name += char
+
+print("Full name in uppercase: ", upper_case_name)
