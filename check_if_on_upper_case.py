ef0065cc371120a8c0512939ee424917a7d911db
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-23 13:45:04 +0800

    check if the letters in the statement if on lowercase

--- check_if_on_upper_case.py
+++ check_if_on_upper_case.py
@@ -3,3 +3,7 @@ example_string = input("Enter any statement: ")
 is_all_caps = True
 found_letter = False
 
+for char in example_string:
+    if 'a' <= char <= 'z':
+        is_all_caps = False
+        break
