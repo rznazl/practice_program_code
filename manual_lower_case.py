3ec5d66e7dcc6e0e6a6cfe2cdc5a22b0023783e7
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-23 13:24:33 +0800

    convert all the letters in lowercase and show the result

--- manual_lower_case.py
+++ manual_lower_case.py
@@ -1,10 +1,13 @@
-full_name = input("Please enter your full name in capital letters.")
+full_name = input("Please enter your full name in capital letters: ")
 
 def manual_lower_case(text):
     result = ""
     for char in text:
         if 'A' <= char <= 'Z':
-            result =+ chr(ord(char) + 32)
+            result += chr(ord(char) + 32)
         else:
             result += char
     return result
+
+lower_case_name = manual_lower_case(full_name)
+print("Your name in lowercase is: ", lower_case_name)
