6309e5a51e543e01a3aa1eb21d3b33b682ac0942
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-23 23:55:19 +0800

    check if any character is in uppercase. also track the characters in lowercase

--- check_if_lower_case.py
+++ check_if_lower_case.py
@@ -1,4 +1,12 @@
 any_statement = input("Enter any text: ")
 
 is_all_lower = True
-found_letter = False 
+found_letter = False
+
+for char in any_statement:
+    if 'A' <= char <= 'Z':
+        is_all_lower = False
+        break
+    if 'a' <= char <= 'z':
+        found_letter = True
+        
