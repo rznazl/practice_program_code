d470b295301ee3d151b48f3d68a0e0620ed97a26
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-23 23:12:21 +0800

    check if the statement ends with the given suffix and ensure that the suffix is not an empty string. print the result

--- remove_suffix.py
+++ remove_suffix.py
@@ -1,3 +1,9 @@
 sample_text = "I love my life"
 given_suffix = "life"
 
+if given_suffix and sample_text.endswith (given_suffix):
+    result = sample_text[:-len(given_suffix)]
+else:
+    result = sample_text
+
+print("Statement without suffix: ", result)
