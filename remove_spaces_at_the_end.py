06524b8cbdab6df249735509154b4011b70d6ded
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-23 23:37:42 +0800

    slice th string from the start up to the last non-space character and print the result

--- remove_spaces_at_the_end.py
+++ remove_spaces_at_the_end.py
@@ -2,5 +2,9 @@ any_statement = input("Enter any string and add spaces at the end: ")
 
 index = len(any_statement) - 1
 
-while index >= 0 and any_statement[index] != " ":
+while index >= 0 and any_statement[index] == " ":
     index -= 1
+
+result = any_statement[:index + 1]
+
+print(f"Output: '{result}'")
