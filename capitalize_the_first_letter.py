b28eb7f8c0efdcfc6a206a020d682db23e12deb7
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-23 21:55:36 +0800

    print the result of the program

--- capitalize_the_first_letter.py
+++ capitalize_the_first_letter.py
@@ -1,7 +1,11 @@
 example_string = input("Enter any text: ")
 
 if example_string:
-    first_part = text[0],upper()
-    rest_of_part = text[1:].lower()
+    first_part = example_string[0].upper()
+    rest_of_part = example_string[1:].lower()
 
     result = first_part + rest_of_part
+else:
+    result = ""
+
+print("Output:", result)
