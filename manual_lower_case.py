cb46a01cd7b945c52ea4cdc806893e9b4b900a3e
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-23 13:18:35 +0800

    check if the character is on uppercase letter then convert to ASCII. keep the spaces

--- manual_lower_case.py
+++ manual_lower_case.py
@@ -1,1 +1,10 @@
 full_name = input("Please enter your full name in capital letters.")
+
+def manual_lower_case(text):
+    result = ""
+    for char in text:
+        if 'A' <= char <= 'Z':
+            result =+ chr(ord(char) + 32)
+        else:
+            result += char
+    return result
