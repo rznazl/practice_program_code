1c40a51b43757bc2980857756f6ce5234796da04
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-23 21:16:06 +0800

    check if the given length is greater than the sample string. print the result

--- print_the_string_at_the_center.py
+++ print_the_string_at_the_center.py
@@ -1,7 +1,15 @@
 sample_string = "Programming"
 given_length = 20
 
-total_padding = width - len(sample_string)
+total_padding = given_length - len(sample_string)
 
 left_padding = total_padding // 2
 right_padding = total_padding - left_padding
+
+if given_length > len(sample_string):
+    result = (" " * left_padding) + sample_string + (" " * right_padding)
+else:
+    result = text
+
+print(f"Output: '{result}'")
+print(f"Total length: {len(result)}")
