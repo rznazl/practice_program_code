8be17e97890135d303478545562dd00ff3d861d3
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-23 22:18:41 +0800

    print the original statement and the converted one

--- first_letter_of_each_word_is_upper_case.py
+++ first_letter_of_each_word_is_upper_case.py
@@ -2,3 +2,5 @@ sample_statement = "I love my life"
 
 converted_statement = " ".join([word[0].upper() + word[1:].lower() for word in sample_statement.split()])
 
+print(f"Original: {sample_statement}")
+print(f"Result: {converted_statement}")
