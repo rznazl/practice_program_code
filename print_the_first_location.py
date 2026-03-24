1cc24e4edc27442a9e78ca1dde9694ff2a3f7cf0
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-25 01:01:39 +0800

    I fixed the code since it results to error. print the location of the part

--- print_the_first_location.py
+++ print_the_first_location.py
@@ -6,6 +6,11 @@ target_to_find_length = len(target_to_find)
 found_index = -1
 
 for i in range(sample_text_length - target_to_find_length + 1):
-    if text[i : i + target_to_find_length] == target:
+    if sample_text[i : i + target_to_find_length] == target:
         found_index = i
         break
+
+if found_index != 1:
+    print(f"The target '{target_to_find}' starts at index :{found_index}")
+else:
+    print("Target found.")
