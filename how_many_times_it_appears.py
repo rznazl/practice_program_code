92994ffca07f965b546f2389359fef0c0a24bbc0
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-24 23:50:45 +0800

    loop through the string, stopping before we run out of characters. slice a window the same size as the target.if the window matches the target, increment the counter

--- how_many_times_it_appears.py
+++ how_many_times_it_appears.py
@@ -5,3 +5,6 @@ any_statement_length = len(any_statement)
 target_letter_length = len(target_letter)
 count = 0
 
+for i in range (any_statement_length- target_letter_length + 1):
+    window = any_statement[i: i + target_letter_length]
+    
