297c95d72c40ebeff16fd46cce379194d6693e7c
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-25 01:16:50 +0800

    put a sample string and the part to find. print the index of the target

--- print_the_last_location.py
+++ print_the_last_location.py
@@ -1,5 +1,10 @@
+
 def manual_rindex(text, target):
     for i in range(len(text) - 1, -1, -1):
         if text[i] == target:
             return i
     raise ValueError("Target not found.")
+
+sample_string = "banana"
+char = "a"
+print(f"The last location of '{char}' is: {manual_rindex(string, char)}")
