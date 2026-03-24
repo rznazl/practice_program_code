fa41f93ea01295d946d84dc1d0d57495662f3b0a
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-24 23:02:14 +0800

    compare the sliced part to the given prefix and print the result of it's true or false

--- string_beginning_matches.py
+++ string_beginning_matches.py
@@ -4,3 +4,10 @@ given_prefix = "Obj"
 prefix_length = len(given_prefix)
 
 beginning_part = sample_string[:prefix_length]
+
+if beginning_part == given_prefix:
+    result = True
+else:
+    result = False
+
+print(f"Starts with '{given_prefix}':", result)
