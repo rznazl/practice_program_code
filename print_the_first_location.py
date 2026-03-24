45fbb118d99202c4105725907fb0a7d99317feb0
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-25 00:53:39 +0800

    find the index of the target part on the given text, make sure to stop at the first occurence

--- print_the_first_location.py
+++ print_the_first_location.py
@@ -5,3 +5,7 @@ sample_text_length = len(sample_text)
 target_to_find_length = len(target_to_find)
 found_index = -1
 
+for i in range(sample_text_length - target_to_find_length + 1):
+    if text[i : i + target_to_find_length] == target:
+        found_index = i
+        break
