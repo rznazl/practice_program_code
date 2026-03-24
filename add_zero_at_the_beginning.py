58ef249fd3d0f8d917ae8afee349c2feb350b144
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-24 23:31:19 +0800

    only add zeroes if the entered string and given length doesn't match. print the result

--- add_zero_at_the_beginning.py
+++ add_zero_at_the_beginning.py
@@ -3,3 +3,9 @@ target_length = int(input("Enter your target length: "))
 
 padding_size = target_length - len(any_statement)
 
+if padding_size > 0:
+    result = ("0" * padding_size) + any_statement
+else:
+    result = any_statement
+    
+print(f"Output: {result}")
