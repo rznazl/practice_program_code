d70655fa3e20cc8f92eeb21dc200dd4e2aa63fcd
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-03-24 23:22:18 +0800

    add spaces at the beginngin only if the length of the sample string doesn't match the given length. print the result

--- add_space_at_the_beginning.py
+++ add_space_at_the_beginning.py
@@ -2,3 +2,11 @@ sample_string = "Love"
 given_length = 15
 
 padding_size = given_length - len(sample_string)
+
+if padding_size > 0:
+    result = (" " * padding_size) + sample_string
+else:
+    result = sample_string
+
+print(f"Output: {result}")
+print(f"Total length: {len(result)}")
